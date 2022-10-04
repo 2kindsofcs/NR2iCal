ebae02b633a06f95a919d82a43add917c20e2f60
Gyusun Yeom <omniavinco@gmail.com>
2022-10-04 22:32:11 +0900

    Refactoring & fix timezone issue

--- src/main.rs
+++ src/main.rs
@@ -1,182 +1,36 @@
-use std::{fmt::Display, io::BufWriter, str::FromStr};
+mod naver_reservation;
+
+use std::{fmt::Display, io::BufWriter};
 
 use anyhow::Context;
-use axum::{extract::Extension, routing::get, Router};
-use chrono::{DateTime, Utc};
+use axum::{extract::Extension, response::IntoResponse, routing::get, Router};
+use chrono::{DateTime, NaiveDateTime, Utc};
 use ics::{components::Property, Event, ICalendar};
-use reqwest::cookie::{CookieStore, Jar};
+use naver_reservation::{FetchOption, UserAuth};
+use reqwest::{header, StatusCode};
 use sqlx::{sqlite::SqliteRow, Row, SqlitePool};
 
-#[derive(Debug, Clone, Copy, serde::Deserialize, serde::Serialize)]
-enum ReservationStatusCode {
-    #[serde(rename = "RC04")]
-    Cancel,
-    #[serde(rename = "RC08")]
-    Completed,
-    #[serde(rename = "RC05")]
-    Reserved,
-}
+type DbExecutor = sqlx::SqlitePool;
 
-impl Display for ReservationStatusCode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let stringified = unsafe { serde_json::to_string(self).unwrap_unchecked() };
-        write!(f, "{}", stringified.trim_matches('"'))
-    }
-}
+static USER_AUTH: once_cell::sync::OnceCell<UserAuth> = once_cell::sync::OnceCell::new();
 
-#[serde_with::serde_as]
-#[derive(serde::Serialize)]
-struct QueryType(
-    #[serde_as(
-        as = "serde_with::StringWithSeparator::<serde_with::formats::CommaSeparator, ReservationStatusCode>"
-    )]
-    Vec<ReservationStatusCode>,
-);
+async fn poll_naver_reservation(db: DbExecutor) -> anyhow::Result<()> {
+    use naver_reservation::ReservationStatusCode;
 
-#[derive(Debug, Clone, serde::Deserialize)]
-struct NaverCalendarResponse {
-    data: Data,
-}
-
-#[derive(Debug, Clone, serde::Deserialize)]
-struct Data {
-    booking: Booking2,
-}
-
-#[derive(Debug, Clone, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Booking2 {
-    bookings: Vec<BookingWrap>,
-    total_count: u32,
-}
-
-#[derive(Debug, Clone, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct BookingWrap {
-    booking_status_code: ReservationStatusCode,
-    is_completed: bool,
-    start_date: chrono::NaiveDate,
-    end_date: chrono::NaiveDate,
-
-    snapshot_json: Booking,
-}
-
-#[derive(Debug, Clone, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Booking {
-    booking_id: i64,
-    // business: any
-    business_id: i64,
-    // business_name: String,
-    service_name: String,
-    // cancelled_date_time: Option<chrono::DateTime<chrono::FixedOffset>>,
-    // completed_date_time: chrono::DateTime<chrono::FixedOffset>,
-    // regDatetime: any
-    #[serde(rename = "bizItemName")]
-    business_item_name: String,
-    #[serde(rename = "bizItemId")]
-    business_item_id: i64,
-    start_date_time: chrono::DateTime<Utc>,
-    end_date_time: chrono::DateTime<Utc>,
-    business_address_json: Address,
-    #[serde(rename = "bookingOptionJson")]
-    options: Vec<ReservationOption>,
-}
-
-#[derive(Debug, Clone, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct ReservationOption {
-    name: String,
-}
-
-#[derive(Debug, Clone, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Address {
-    road_addr: String,
-}
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    env_logger::init();
-
-    let db_pool = sqlx::SqlitePool::connect("./data.sqlite").await?;
-    sqlx::migrate!().run(&db_pool).await?;
-
-    const ENDPOINT: &str = "https://m.booking.naver.com/graphql";
-    let cookie_url = reqwest::Url::from_str(ENDPOINT).unwrap();
-    let jar = Jar::default();
-    for cookie_name in ["NID_AUT", "NID_SES"] {
-        let value = std::env::var(cookie_name)?;
-        let cookie = format!("{}={}", cookie_name, value);
-        jar.add_cookie_str(&cookie, &cookie_url);
-    }
-
-    let payload = serde_json::json!({
-        "operationName": "bookings",
-        "variables": {
-            "input": {
-                "queryType": QueryType(vec![
-                    ReservationStatusCode::Cancel,
-                    ReservationStatusCode::Completed,
-                    ReservationStatusCode::Reserved
-                ]),
-                "businessMainCategory": "ALL",
-                "startDate": null,
-                "endDate": null,
-                "size": 4,
-                "page": 0,
-            },
+    let res = naver_reservation::fetch(
+        unsafe { USER_AUTH.get_unchecked() },
+        FetchOption {
+            query_types: vec![
+                ReservationStatusCode::Cancel,
+                ReservationStatusCode::Completed,
+                ReservationStatusCode::Reserved,
+            ],
+            size: 10,
+            ..Default::default()
         },
-        "query": r#"query bookings($input: BookingParams) {
-            booking(input: $input) {
-              id
-              totalCount
-              bookings {
-                bookingId
-                businessName
-                serviceName
-                bookingStatusCode
-                isCompleted
-                startDate
-                endDate
-                regDateTime
-                completedDateTime
-                cancelledDateTime
-                snapshotJson
-                business {
-                  addressJson
-                  completedPinValue
-                  name
-                  serviceName
-                  isImp
-                  isDeleted
-                  isCompletedButtonImp
-                  phoneInformationJson
-                }
-              }
-            }
-          }
-          "#,
-    });
-    println!("{:#?}", serde_json::to_string(&payload).unwrap());
+    )
+    .await?;
 
-    let client = reqwest::Client::new();
-    let req = client
-        .post(ENDPOINT)
-        .header(reqwest::header::COOKIE, jar.cookies(&cookie_url).unwrap())
-        .header(reqwest::header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15")
-        .json(&payload)
-        .build()?;
-    println!("{:?}", &req);
-
-    let res = client.execute(req).await?;
-    let res = res.bytes().await?;
-    let res: NaverCalendarResponse = serde_json::from_slice(&res).with_context(|| {
-        format!("Failed to parse\n{}", unsafe {
-            std::str::from_utf8_unchecked(&res)
-        })
-    })?;
-    // println!("{:#?}", &res);
     for reservation in res.data.booking.bookings {
         let reservation = reservation.snapshot_json;
         let options = reservation
@@ -209,12 +63,31 @@ async fn main() -> anyhow::Result<()> {
             options,
             reservation.business_address_json.road_addr,
         )
-        .execute(&db_pool)
+        .execute(&db)
         .await?;
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let db_pool = sqlx::SqlitePool::connect("./data.sqlite").await?;
+    sqlx::migrate!().run(&db_pool).await?;
+
+    let user = UserAuth {
+        aut: std::env::var("NID_AUT")?,
+        ses: std::env::var("NID_SES")?,
+    };
+    if USER_AUTH.set(user).is_err() {
+        return Err(anyhow::anyhow!("Impossible initialization logic error"));
     }
 
     let app = Router::new()
         .route("/", get(get_all_reservation))
+        .route("/fetch", get(fetch_naver_reservation))
         .layer(Extension(db_pool));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -225,6 +98,13 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+async fn fetch_naver_reservation(Extension(db_pool): Extension<SqlitePool>) {
+    if let Err(e) = poll_naver_reservation(db_pool).await {
+        log::error!("Failed to fetch naver reservation - {}", e);
+    }
+}
+
+#[derive(Debug)]
 struct Reservation {
     id: i64,
     business_name: String,
@@ -238,14 +118,16 @@ impl sqlx::FromRow<'_, SqliteRow> for Reservation {
     fn from_row(row: &'_ SqliteRow) -> Result<Self, sqlx::Error> {
         let raw_options: String = row.try_get("options")?;
 
+        let start_date_time: NaiveDateTime = row.try_get("start_date_time")?;
+        let end_date_time: NaiveDateTime = row.try_get("end_date_time")?;
         Ok(Self {
             id: row.try_get("id")?,
             business_name: row.try_get("business_name")?,
             item_name: row.try_get("item_name")?,
             options: serde_json::from_str(&raw_options)
                 .map_err(|e| sqlx::Error::Decode(Box::new(e)))?,
-            start_date_time: row.try_get("id")?,
-            end_date_time: row.try_get("id")?,
+            start_date_time: DateTime::from_utc(start_date_time, Utc),
+            end_date_time: DateTime::from_utc(end_date_time, Utc),
         })
     }
 }
@@ -266,14 +148,14 @@ impl From<Reservation> for Event<'static> {
             format!("{} - {}", reservation.business_name, reservation.item_name),
         ));
         event.push(Property::new("DESCRIPTION", reservation.options.join("\n")));
-        event.push(Property::new("DTSTART", start_date));
-        event.push(Property::new("DTEND", end_date));
+        event.push(Property::new("DTSTART;TZID=Etc/UTC", start_date));
+        event.push(Property::new("DTEND;TZID=Etc/UTC", end_date));
 
         event
     }
 }
 
-async fn get_all_reservation(Extension(db_pool): Extension<SqlitePool>) -> String {
+async fn reservation_to_ics(db_pool: DbExecutor) -> anyhow::Result<String> {
     let reservations: Vec<Reservation> = sqlx::query_as(
         r#"SELECT
         id, business_name, item_name, options,
@@ -281,16 +163,38 @@ async fn get_all_reservation(Extension(db_pool): Extension<SqlitePool>) -> Strin
         end_date_time FROM reservation"#,
     )
     .fetch_all(&db_pool)
-    .await
-    .unwrap();
+    .await?;
     let mut calendar = ICalendar::new("2.0", "kawai");
+    calendar.push(Property::new("X-WR-TIMEZONE", "Etc/UTC"));
+    calendar.add_timezone(ics::TimeZone::standard(
+        "Etc/UTC",
+        ics::Standard::new("19700101T000000", "0000", "0000"),
+    ));
     for reservation in reservations {
         calendar.add_event(reservation.into());
     }
 
     let mut writer = BufWriter::new(Vec::new());
     calendar.write(&mut writer).unwrap();
-    let buffer = writer.into_inner().unwrap();
+    let buffer = writer.into_inner()?;
 
-    String::from_utf8(buffer).unwrap()
+    Ok(String::from_utf8(buffer)?)
+}
+
+async fn get_all_reservation(Extension(db_pool): Extension<SqlitePool>) -> impl IntoResponse {
+    match reservation_to_ics(db_pool).await {
+        Ok(body) => (
+            StatusCode::OK,
+            [(header::CONTENT_TYPE, "text/calendar")],
+            body,
+        ),
+        Err(e) => {
+            log::error!("Failed to generate ics - {}", e);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                [(header::CONTENT_TYPE, "text/plain")],
+                "".to_string(),
+            )
+        }
+    }
 }
---
+++ src/naver_reservation.rs
@@ -0,0 +1,190 @@
+use std::fmt::Display;
+
+use anyhow::Context;
+use chrono::{DateTime, NaiveDate, Utc};
+use reqwest::cookie::{CookieStore, Jar};
+
+#[derive(Debug, Clone, Copy, serde::Deserialize, serde::Serialize)]
+pub enum ReservationStatusCode {
+    #[serde(rename = "RC04")]
+    Cancel,
+    #[serde(rename = "RC08")]
+    Completed,
+    #[serde(rename = "RC05")]
+    Reserved,
+}
+
+impl Display for ReservationStatusCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let stringified = unsafe { serde_json::to_string(self).unwrap_unchecked() };
+        write!(f, "{}", stringified.trim_matches('"'))
+    }
+}
+
+#[serde_with::serde_as]
+#[derive(serde::Serialize)]
+struct QueryType(
+    #[serde_as(
+        as = "serde_with::StringWithSeparator::<serde_with::formats::CommaSeparator, ReservationStatusCode>"
+    )]
+    Vec<ReservationStatusCode>,
+);
+
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct NaverCalendarResponse {
+    pub data: Data,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct Data {
+    pub booking: Booking2,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Booking2 {
+    pub bookings: Vec<BookingWrap>,
+    pub total_count: u32,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BookingWrap {
+    pub booking_status_code: ReservationStatusCode,
+    pub is_completed: bool,
+    pub start_date: NaiveDate,
+    pub end_date: NaiveDate,
+
+    pub snapshot_json: Booking,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Booking {
+    pub booking_id: i64,
+    // pub business: any
+    pub business_id: i64,
+    // pub business_name: String,
+    pub service_name: String,
+    // pub cancelled_date_time: Option<chrono::DateTime<chrono::FixedOffset>>,
+    // pub completed_date_time: chrono::DateTime<chrono::FixedOffset>,
+    // pub regDatetime: any
+    #[serde(rename = "bizItemName")]
+    pub business_item_name: String,
+    #[serde(rename = "bizItemId")]
+    pub business_item_id: i64,
+    pub start_date_time: DateTime<Utc>,
+    pub end_date_time: DateTime<Utc>,
+    pub business_address_json: Address,
+    #[serde(rename = "bookingOptionJson")]
+    pub options: Vec<ReservationOption>,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ReservationOption {
+    pub name: String,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Address {
+    pub road_addr: String,
+}
+
+const ENDPOINT: once_cell::sync::Lazy<reqwest::Url> = once_cell::sync::Lazy::new(|| {
+    use std::str::FromStr;
+    unsafe { reqwest::Url::from_str("https://m.booking.naver.com/graphql").unwrap_unchecked() }
+});
+
+pub struct UserAuth {
+    pub aut: String,
+    pub ses: String,
+}
+
+impl UserAuth {
+    fn to_cookie_jar(&self) -> Jar {
+        let jar = Jar::default();
+        jar.add_cookie_str(&format!("{}={}", "NID_AUT", self.aut), &ENDPOINT);
+        jar.add_cookie_str(&format!("{}={}", "NID_SES", self.ses), &ENDPOINT);
+        jar
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct FetchOption {
+    pub query_types: Vec<ReservationStatusCode>,
+    pub start_date: Option<DateTime<Utc>>,
+    pub end_date: Option<DateTime<Utc>>,
+    pub size: usize,
+    pub page: usize,
+}
+
+pub async fn fetch(
+    user_auth: &UserAuth,
+    option: FetchOption,
+) -> anyhow::Result<NaverCalendarResponse> {
+    let payload = serde_json::json!({
+        "operationName": "bookings",
+        "variables": {
+            "input": {
+                "queryType": QueryType(option.query_types),
+                "businessMainCategory": "ALL",
+                "startDate": option.start_date,
+                "endDate": option.end_date,
+                "size": option.size,
+                "page": option.page,
+            },
+        },
+        "query": r#"query bookings($input: BookingParams) {
+            booking(input: $input) {
+              id
+              totalCount
+              bookings {
+                bookingId
+                businessName
+                serviceName
+                bookingStatusCode
+                isCompleted
+                startDate
+                endDate
+                regDateTime
+                completedDateTime
+                cancelledDateTime
+                snapshotJson
+                business {
+                  addressJson
+                  completedPinValue
+                  name
+                  serviceName
+                  isImp
+                  isDeleted
+                  isCompletedButtonImp
+                  phoneInformationJson
+                }
+              }
+            }
+          }
+          "#,
+    });
+
+    let jar = user_auth.to_cookie_jar();
+
+    let client = reqwest::Client::new();
+    let req = client
+        .post(ENDPOINT.as_ref())
+        .header(reqwest::header::COOKIE, jar.cookies(&ENDPOINT).unwrap())
+        .header(reqwest::header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15")
+        .json(&payload)
+        .build()?;
+
+    let res = client.execute(req).await?;
+    let res = res.bytes().await?;
+    let res: NaverCalendarResponse = serde_json::from_slice(&res).with_context(|| {
+        format!("Failed to parse\n{}", unsafe {
+            std::str::from_utf8_unchecked(&res)
+        })
+    })?;
+
+    Ok(res)
+}
